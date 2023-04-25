a7417e36114dcbf7e4b5bbf3c0cc200079c78a6a
radhey-molsoft <himali.baps@gmail.com>
2023-04-25 14:42:08 -0400

    api routes delete function added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,3 +19,15 @@ router.post('/notes', (req, res) => {
       .then((note) => res.json(note))
       .catch(err => res.status(500).json(err));
 });
+
+
+// DELETE
+router.delete('/notes/:id', function (req, res) {
+  saveData
+      .deleteNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch(err => res.status(500).json(err));
+});
+
+
+module.exports = router;
