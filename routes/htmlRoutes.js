c8541a91db9753aa1e99be9f2fe66aae196fc4c1
radhey-molsoft <himali.baps@gmail.com>
2023-04-25 15:01:13 -0400

    index and home page added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,3 +6,16 @@ const router = require('express').Router();
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
+
+//index html route
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+// home page if nothing found
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
