6f8d2aa4fdbb08539b342af99de74e10ec213b88
radhey-molsoft <himali.baps@gmail.com>
2023-04-25 14:30:14 -0400

    post added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,3 +10,12 @@ router.get('/notes', function (req, res) {
         .then(notes => res.json(notes))
         .catch(err => res.status(500).json(err));
 });
+
+
+// POST
+router.post('/notes', (req, res) => {
+  saveData
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch(err => res.status(500).json(err));
+});
